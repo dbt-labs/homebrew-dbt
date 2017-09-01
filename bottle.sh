8ad4fb0e16390c0c509345ef785aabf90d42cbd0
Connor McArthur <cmcarthur@rjmetrics.com>
2017-09-01 17:28:10 -0400

    use tap

--- bottle.sh
+++ bottle.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+FORMULA="fishtown-analytics/dbt/dbt@0.8.3"
+
 brew test-bot --ci-testing
-brew install --build-bottle dbt@0.8.3
-brew bottle --json dbt@0.8.3
-brew bottle --merge --write --no-commit dbt@0.8.3-0.8.3.sierra.bottle.json
-brew uninstall --force dbt@0.8.3
-brew install ./dbt@0.8.3-0.8.3.sierra.bottle.tar.gz
-brew test dbt@0.8.3 --verbose
+brew install --build-bottle "$FORMULA"
+brew bottle --json "$FORMULA"
+brew bottle --merge --write --no-commit "$FORMULA-0.8.3.sierra.bottle.json"
+brew uninstall --force "$FORMULA"
+brew install './'"$FORMULA"'-0.8.3.sierra.bottle.tar.gz'
+brew test "$FORMULA" --verbose
