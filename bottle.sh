56c0bec93426ac3fa354c60c0c4f631da7ca9389
Connor McArthur <cmcarthur@rjmetrics.com>
2017-09-01 16:55:42 -0400

    set up bottle cmds

--- bottle.sh
+++ bottle.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+brew test-bot --ci-testing
+brew install --build-bottle dbt@0.8.3
+brew bottle --json dbt@0.8.3
+brew bottle --merge --write --no-commit dbt@0.8.3-0.8.3.sierra.bottle.json
+brew uninstall --force dbt@0.8.3
+brew install ./dbt@0.8.3-0.8.3.sierra.bottle.tar.gz
+brew test dbt@0.8.3 --verbose
