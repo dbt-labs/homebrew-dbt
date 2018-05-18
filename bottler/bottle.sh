d7b7b839218773068ff6379ed01ceb2cf93ac930
Connor McArthur <cmcarthur@rjmetrics.com>
2018-05-18 15:27:27 -0400

    el cap no more

--- bottler/bottle.sh
+++ bottler/bottle.sh
@@ -33,12 +33,12 @@ packer build \
        packer.json || exit 1
 
 # EL CAPITAN 10.11
-rm -rf output-virtualbox-ovf
-packer build \
-       -var provisioning-delay=30 \
-       -var "formula_name=$FORMULA_NAME" \
-       -var "dbt_version_string=$VERSION" \
-       -var "rebuild_string=$REBUILD_STRING" \
-       -var macos_version=el_capitan \
-       -var source_path=images/macOS_10.11.ovf \
-       packer.json || exit 1
+# rm -rf output-virtualbox-ovf
+# packer build \
+#        -var provisioning-delay=30 \
+#        -var "formula_name=$FORMULA_NAME" \
+#        -var "dbt_version_string=$VERSION" \
+#        -var "rebuild_string=$REBUILD_STRING" \
+#        -var macos_version=el_capitan \
+#        -var source_path=images/macOS_10.11.ovf \
+#        packer.json || exit 1
