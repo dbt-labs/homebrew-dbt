18bdb42e0ac89db01f53cc11caf2958499d67cb3
Connor McArthur <connor@fishtownanalytics.com>
2018-08-27 09:21:49 -0400

    0.11.0-a1 bottled

--- bottler/generate-bottle-clause.sh
+++ bottler/generate-bottle-clause.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PREFIX='dbt-0.10.1'
+PREFIX='dbt@0.11.0-a1--0.11.0a1'
 
 HIGH_SIERRA="$(cat "build/$PREFIX.high_sierra.bottle.json" | jq '. | to_entries[] | .value.bottle.tags | to_entries[] | .value.sha256')"' => :high_sierra'
 SIERRA="$(cat "build/$PREFIX.sierra.bottle.json" | jq '. | to_entries[] | .value.bottle.tags | to_entries[] | .value.sha256')"' => :sierra'
