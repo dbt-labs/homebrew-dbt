aa3ee6a5536ee4b97852218b3c85717004c4f0ec
leahwicz <60146280+leahwicz@users.noreply.github.com>
2022-01-05 20:51:38 -0500

    Updating snowflake 1.0.0 to use core 1.0.1

--- Formula/dbt-snowflake@1.0.0.rb
+++ Formula/dbt-snowflake@1.0.0.rb
@@ -5,6 +5,7 @@ class DbtSnowflakeAT100 < Formula
   homepage "https://github.com/dbt-labs/dbt-snowflake"
   url "https://files.pythonhosted.org/packages/76/ab/de915468520e061bd2e712a0ce45e7568b06547b6f65321d9ebc7018c2ec/dbt-snowflake-1.0.0.tar.gz"
   sha256 "a263274d6af430edfe33cf57b44c7eba58a73017ec8b1c82cb30b25e42be9a1c"
+  revision 1
 
   depends_on "python3"
   depends_on "rust" => :build
@@ -60,8 +61,8 @@ class DbtSnowflakeAT100 < Formula
   end
 
   resource "dbt-core" do
-    url "https://files.pythonhosted.org/packages/9b/c9/a54e72b7defd4fe7b6e75f3b8791642ef0b7f64759bb2c18a59a8fbe2e5a/dbt-core-1.0.0.tar.gz"
-    sha256 "90bde01d078b1d673dc490b9defff07e7f3b702b920c65feae6fb27e07ce73d2"
+    url "https://files.pythonhosted.org/packages/30/f8/5d1efef6ba713d5f3cd0166754596ab4fb73ae43a46bcfa8916a95c80b5e/dbt-core-1.0.1.tar.gz"
+    sha256 "0547c45f6c854399a8bcb61eca3ccdab1e1e9c99cd0ecaca22de293cb7ee2285"
   end
 
   resource "dbt-extractor" do
