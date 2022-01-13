4c503ed75d50510074b6514f4026724ee704d15f
Kyle Wigley <kyle@dbtlabs.com>
2022-01-13 17:25:27 -0500

    use native git client when installing dbt-extractor from source istead of the git client built into cargo

--- Formula/dbt-bigquery@1.0.0-b1.rb
+++ Formula/dbt-bigquery@1.0.0-b1.rb
@@ -285,6 +285,7 @@ class DbtBigqueryAT100B1 < Formula
   end
 
   def install
+    ENV["CARGO_NET_GIT_FETCH_WITH_CLI"] = "true"
     venv = virtualenv_create(libexec, "python3")
     venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/pip", "install",
       "--upgrade", "pip"
