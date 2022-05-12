4babbfb8fc260f48f67a2cd5d532c32a9f28a95a
Chenyu Li <chenyu.li@dbtlabs.com>
2022-05-12 10:29:23 -0700

    Pin pip for all adapters (#122)

--- Formula/dbt-snowflake.rb
+++ Formula/dbt-snowflake.rb
@@ -273,8 +273,8 @@ class DbtSnowflake < Formula
   def install
     ENV["CARGO_NET_GIT_FETCH_WITH_CLI"] = "true"
     venv = virtualenv_create(libexec, "python3")
-    venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/pip", "install",
-      "--upgrade", "pip"
+    venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/python", "-m", "pip", "install",
+      "pip==22.0.4"
     resources.each do |r|
       if r.name == "snowflake-connector-python"
         # workaround for installing `snowflake-connector-python`
