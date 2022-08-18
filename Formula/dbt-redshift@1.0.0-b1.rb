bea9e3aac05b3ca9e3017c0ce59b51eaf0c17a3f
Chenyu Li <chenyu.li@dbtlabs.com>
2022-08-18 15:46:34 -0700

    stop using system site package

--- Formula/dbt-redshift@1.0.0-b1.rb
+++ Formula/dbt-redshift@1.0.0-b1.rb
@@ -246,7 +246,7 @@ class DbtRedshiftAT100B1 < Formula
 
   def install
     ENV["CARGO_NET_GIT_FETCH_WITH_CLI"] = "true"
-    venv = virtualenv_create(libexec, "python3")
+    venv = virtualenv_create(libexec, "python3", system_site_packages: false)
     venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/pip", "install",
       "--upgrade", "pip"
 
