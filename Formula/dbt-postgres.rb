12b314feca10a0a3981239adbf518796895031a0
Emily Rockman <emily.rockman@dbtlabs.com>
2023-01-05 21:49:23 -0600

    test python version

--- Formula/dbt-postgres.rb
+++ Formula/dbt-postgres.rb
@@ -223,7 +223,7 @@ class DbtPostgres < Formula
 
   def install
     ENV["CARGO_NET_GIT_FETCH_WITH_CLI"] = "true"
-    venv = virtualenv_create(libexec, "python3", system_site_packages: false)
+    venv = virtualenv_create(libexec, "python", system_site_packages: false)
     venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/python",
        "-m", "pip", "install", "pip==22.3.1"
     resources.each do |r|
