710873b7f985e01764093bc6b4c411f033a9bdf4
Mike Alfare <13974384+mikealfare@users.noreply.github.com>
2023-12-18 14:33:46 -0500

    Configure dependabot for GHA (#361)
    * configure dependabot for gha
    * add setup.py
    * update .gitignore

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="homebrew-dbt",
+    author="dbt Labs",
+    author_email="info@dbtlabs.com",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.8",
+)
