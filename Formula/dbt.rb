450102141dbc25756e583e72080e86923443b7cf
fa-assistant <77991023+fa-assistant@users.noreply.github.com>
2026-08-05 01:15:35 +0000

    dbt 2.0.0-preview.205

--- Formula/dbt.rb
+++ Formula/dbt.rb
@@ -1,28 +1,28 @@
 class Dbt < Formula
   desc "Build analytics the way engineers build applications"
   homepage "https://getdbt.com"
-  version "2.0.0-preview.204"
+  version "2.0.0-preview.205"
   license :cannot_represent
 
   on_macos do
     on_arm do
-      url "https://public.cdn.getdbt.com/fs/cli/fs-v2.0.0-preview.204-aarch64-apple-darwin.tar.gz"
-      sha256 "591acb36c37758e9737122308ee7a1e568e2bd609894ab6cad6ffadb6237d928"
+      url "https://public.cdn.getdbt.com/fs/cli/fs-v2.0.0-preview.205-aarch64-apple-darwin.tar.gz"
+      sha256 "30bc15b0ca6b8fd37870850c3b8c3a487e830c09b06dbc62345dde25981a009a"
     end
     on_intel do
-      url "https://public.cdn.getdbt.com/fs/cli/fs-v2.0.0-preview.204-x86_64-apple-darwin.tar.gz"
-      sha256 "6757416a1634f53b3077f9c07f7e6a6136df0577f007f9642ac4fa685028fa2f"
+      url "https://public.cdn.getdbt.com/fs/cli/fs-v2.0.0-preview.205-x86_64-apple-darwin.tar.gz"
+      sha256 "e7a56eb7bb8c47556afcd8586c517e4350a5b5d03786a12c8b71519a8e09bd95"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://public.cdn.getdbt.com/fs/cli/fs-v2.0.0-preview.204-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "2545fb7cd9169c0e749b8f4b420c01bdec6df6fcde9e9397bf3cc1e086932ca4"
+      url "https://public.cdn.getdbt.com/fs/cli/fs-v2.0.0-preview.205-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "7787ed0e8a9301f7a71973dc355a80934c0c1ef61e9faef9dffffb7769f61c97"
     end
     on_intel do
-      url "https://public.cdn.getdbt.com/fs/cli/fs-v2.0.0-preview.204-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "cbf207addbc20285b59e9b2628e7dad429244685c88441246a1034ef88f278b7"
+      url "https://public.cdn.getdbt.com/fs/cli/fs-v2.0.0-preview.205-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "60e476cc56aa11fc66f898b23a0cc4af24912329ea9c22fa565c353b1abfb6dc"
     end
   end
 
